a7ed10cf78cf1229752e9c491c165254efc3292d
Adam0 <alumail@protonmail.com>
2026-09-04 21:53:12 +0200

    Use main Nixpkgs for flake-parts

--- flake.nix
+++ flake.nix
@@ -8,7 +8,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
 
     import-tree.url = "github:vic/import-tree";
 
